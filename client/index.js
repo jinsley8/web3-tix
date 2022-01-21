d861907a97296de01cd64a2aaeb638b0b0f61c4b
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-01-21 02:34:14 -0500

    feat: Retrieve tickets from contract, display in grid

--- client/index.js
+++ client/index.js
@@ -18,14 +18,45 @@ const web3 = new Web3(
 
 const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
+const TOTAL_TICKETS = 12;
+const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
+
 let account;
 
 const accountEl = document.getElementById('account');
+const ticketsEl = document.getElementById('tickets');
+
+const refreshTickets = async () => {
+  ticketsEl.innerHTML = '';
+  for (let i = 0; i < TOTAL_TICKETS; i++) {
+    const ticket = await contract.methods.tickets(i).call();
+    console.log(ticket);
+    ticket.id = i;
+
+    if (ticket.owner === EMPTY_ADDRESS) {
+      const ticketEl = createElementFromString(`
+        <div class="col-sm mb-4">
+          <div class="card" style="width: 18rem;">
+            <div class="card-img-top p-4" style="background-color:#F6F7F9;">
+              <img style="width:100%; height: auto;" src=${ticketImg} alt="Ticket image">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">${ticket.price/1e18} ETH</h5>
+              <button class="btn btn-primary">Buy</button>
+            </div>
+          </div>
+        </div>
+      `);
+      ticketsEl.appendChild(ticketEl);
+    }
+  }
+}
 
 const main = async () => {
   const accounts = await web3.eth.requestAccounts();
   account = accounts[0];
   accountEl.innerText = account;
+  await refreshTickets();
 }
 
 main();
